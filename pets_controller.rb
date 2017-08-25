88a5f5e8d2ed15194302abb3e3d467ee343c2867
Robert James Bowes <up666724@myport.ac.uk>
2017-08-25 16:18:06 +0100

    Before creating adoption class

--- pets_controller.rb
+++ pets_controller.rb
@@ -6,46 +6,51 @@ require_relative('./models/pet.rb')
 
 also_reload(".models/*")
 
-# INDEX ROUTE
-get '/jurassic-bark' do
+get '/' do
   erb(:index)
 end
 
+# # INDEX ROUTE
+# get '/pets' do
+#
+# end
+
 # READ ROUTE
-get '/jurassic-bark/all' do
+get '/pets' do
   @pets = Pet.all
   erb(:all)
 end
 
-get '/jurassic-bark/:id' do
+get '/pets/new' do
+  @pets = Pet.all
+  erb(:new)
+end
+
+get '/pets/:id' do
   @pet = Pet.find_by_id(params[:id])
   erb(:show)
 end
 
 # CREATE ROUTE
-get '/jurassic-bark/new' do
-  @pets = Pet.all
-  erb(:new)
-end
 
-post '/jurassic-bark/all' do
+post '/pets' do
   Pet.new(params).save
-  redirect '/jurassic-bark/all'
+  redirect '/pets'
 end
 
-get 'jurassic-bark/:id/edit' do
+get 'pets/:id/edit' do
   @pet = Pet.find_by_id(params['id'])
   erb(:edit)
 end
 
-post 'jurassic-bark/:id' do
+post 'pets/:id' do
   pet = Pet.new(params)
   pet.update()
-  redirect to "/jurassic-bark/#{params['id']}"
+  redirect to "/pets/#{params['id']}"
 end
 
-post '/jurassic-bark/:id/delete' do
+post '/pets/:id/delete' do
   pet = Pet.find_by_id(params['id'])
   pet.delete
-  redirect to '/jurassic-bark/all'
+  redirect to '/pets/all'
 end
